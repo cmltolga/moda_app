f90cb09e87a77904536cfeea77ae34a4a8ff3c57
cmltolga <tolgacml58@gmail.com>
2021-11-22 16:03:48 +0300

    moda app design

---
+++ lib/main.dart
@@ -0,0 +1,469 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_moda_app/detay.dart';
+
+void main() => runApp(ModaApp());
+
+class ModaApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnaSayfa(),
+    );
+  }
+}
+
+class AnaSayfa extends StatefulWidget {
+  @override
+  _AnaSayfaState createState() => _AnaSayfaState();
+}
+
+class _AnaSayfaState extends State<AnaSayfa>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+          indicatorColor: Colors.transparent,
+          controller: tabController,
+          tabs: [
+            Tab(
+              icon: Icon(
+                Icons.more,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.play_arrow,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.navigation,
+                color: Colors.grey,
+                size: 22,
+              ),
+            ),
+            Tab(
+              icon: Icon(
+                Icons.supervised_user_circle,
+                color: Colors.grey,
+                size: 22,
+              ),
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          "Moda Uygulaması",
+          style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Montserrat',
+              fontSize: 22,
+              fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.camera_alt),
+            onPressed: () {},
+            color: Colors.grey,
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(top: 10),
+        children: [
+          Container(
+            //color: Colors.blue,
+            height: 140,
+            width: double.infinity,
+            child: ListView(
+              padding: EdgeInsets.all(10),
+              scrollDirection: Axis.horizontal,
+              children: [
+                listeElemani('assets/images/model1.jpeg',
+                    'assets/images/chanellogo.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/images/model2.jpeg',
+                    'assets/images/louisvuitton.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani(
+                    'assets/images/model3.jpeg', 'assets/images/chloelogo.png'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/images/model1.jpeg',
+                    'assets/images/chanellogo.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani('assets/images/model2.jpeg',
+                    'assets/images/louisvuitton.jpg'),
+                SizedBox(
+                  width: 30,
+                ),
+                listeElemani(
+                    'assets/images/model3.jpeg', 'assets/images/chloelogo.png'),
+                SizedBox(
+                  width: 30,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              elevation: 4,
+              //color: Colors.blue.shade300,
+              child: Container(
+                height: 500,
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/model1.jpeg'),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width - 160,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Daisy',
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                "34 minutes ago",
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat',
+                                    fontSize: 12,
+                                    color: Colors.grey),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(
+                          Icons.more_vert,
+                          color: Colors.grey,
+                          size: 22,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas congue semper turpis, vel vehicula ipsum hendrerit ut.",
+                      style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Montserrat',
+                          color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => Detay(
+                                      imgPath:
+                                          'assets/images/modelgrid1.jpeg')));
+                            });
+                          },
+                          child: Hero(
+                            tag: 'assets/images/modelgrid1.jpeg',
+                            child: Container(
+                              width:
+                                  (MediaQuery.of(context).size.width - 50) / 2,
+                              height: 200,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        'assets/images/modelgrid1.jpeg'),
+                                    fit: BoxFit.cover),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => Detay(
+                                          imgPath:
+                                              'assets/images/modelgrid2.jpeg')));
+                                });
+                              },
+                              child: Hero(
+                                tag: 'assets/images/modelgrid2.jpeg',
+                                child: Container(
+                                  width: (MediaQuery.of(context).size.width -
+                                          100) /
+                                      2,
+                                  height: 95,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/images/modelgrid2.jpeg'),
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => Detay(
+                                          imgPath:
+                                              'assets/images/modelgrid3.jpeg')));
+                                });
+                              },
+                              child: Hero(
+                                tag: 'assets/images/modelgrid3.jpeg',
+                                child: Container(
+                                  width: (MediaQuery.of(context).size.width -
+                                          100) /
+                                      2,
+                                  height: 95,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/images/modelgrid3.jpeg'),
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          height: 25,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.brown.withOpacity(0.2),
+                          ),
+                          child: Center(
+                              child: Text(
+                            "# Louis vuitton",
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 10,
+                                color: Colors.brown),
+                          )),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          height: 25,
+                          width: 75,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.brown.withOpacity(0.2),
+                          ),
+                          child: Center(
+                              child: Text(
+                            "# Chloe",
+                            style: TextStyle(
+                                fontFamily: 'Montserrat',
+                                fontSize: 10,
+                                color: Colors.brown),
+                          )),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Divider(),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.reply,
+                          color: Colors.brown.withOpacity(0.4),
+                          size: 30,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "1.7k",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                        SizedBox(
+                          width: 25,
+                        ),
+                        Icon(
+                          Icons.comment,
+                          color: Colors.brown.withOpacity(0.4),
+                          size: 30,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "325",
+                          style:
+                              TextStyle(fontFamily: 'Montserrat', fontSize: 16),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width - 235,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                                size: 30,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                "2.3k",
+                                style: TextStyle(
+                                    fontFamily: 'Montserrat', fontSize: 16),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  listeElemani(String imagePath, String logoPath) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: 75,
+              width: 75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(38),
+                image: DecorationImage(
+                    image: AssetImage(imagePath), fit: BoxFit.cover),
+              ),
+            ),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                      image: AssetImage(logoPath), fit: BoxFit.cover),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          height: 30,
+          width: 75,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.brown),
+          child: Center(
+              child: Text(
+            "Follow",
+            style: TextStyle(
+                fontFamily: 'Montserrat', fontSize: 14, color: Colors.white),
+          )),
+        ),
+      ],
+    );
+  }
+}
